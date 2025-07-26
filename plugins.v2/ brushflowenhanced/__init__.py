cbe344f1cc12ff5e3ccde8fab1dc2c28b894eeec
shaohan <im@shaohan.cc>
2025-07-27 01:14:28 +0800

    test

--- plugins.v2/ brushflowenhanced/__init__.py
+++ plugins.v2/ brushflowenhanced/__init__.py
@@ -271,7 +271,7 @@ class BrushFlowEnhanced(_PluginBase):
     # 插件图标
     plugin_icon = "brush.jpg"
     # 插件版本
-    plugin_version = "4.3.3.1"
+    plugin_version = "9.9.9"
     # 插件作者
     plugin_author = "shaohan"
     # 作者主页
